f3e82db1ae98af7f9a6a57b4d1af3bbe135085c0
cool21th <cool21th@naver.com>
2018-10-26 11:35:30 +0900

    Create part2_pre_calculate.py

---
+++ tutorial/image_caption/data/part2_pre_calculate.py
@@ -0,0 +1,35 @@
+from os import listdir
+from pickle import dump
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.layers import Input
+
+
+# Extract features from each photo in the directory
+def extract_features(directory):
+    in_layer = Input(shape=(224,224,3))
+    model = VGG16(include_top=False, input_tensor=in_layer)
+    print(model.summary())
+    features =dict()
+    for name in listdir(directory):
+        filename = directory + '/' + name
+        image = load_img(filename, target_size=(224, 224))
+        image = img_to_array(image)
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        image = preprocess_input(image)
+        feature = model.predict(image, verbose=0)
+        image_id = name.split('.')[0]
+        features[image_id] = feature
+        print('>%s' % name)
+    return features
+
+
+# extract features from all images
+directory = 'Flicker8K_Dataset'
+features = extract_features(directory)
+print('Extracted Features: %d:' %len(features))
+
+# save file
+dump(features, open('features.pkl', 'wb'))
